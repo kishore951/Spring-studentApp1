ea685145916b8b6337b6b34a2949d6c69fcc4243
kishore951 <kishoresakthivel951@gmail.com>
2022-10-15 15:00:15 +0530

    new

---
+++ src/main/java/com/nestdigital/studentApp1/StudentApp1Application.java
@@ -0,0 +1,13 @@
+package com.nestdigital.studentApp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentApp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestdigital/studentApp1/StudentApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.studentApp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
